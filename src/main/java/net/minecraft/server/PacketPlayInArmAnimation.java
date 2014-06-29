e9ffe7cf6247000e09652badd6b34085d1412247
Thinkofdeath <thinkofdeath@spigotmc.org>
2014-06-29 21:10:34 +0100

    Limit block placement/interaction packets

--- src/main/java/net/minecraft/server/PacketPlayInArmAnimation.java
+++ src/main/java/net/minecraft/server/PacketPlayInArmAnimation.java
@@ -4,9 +4,13 @@ import java.io.IOException;
 
 public class PacketPlayInArmAnimation implements Packet<PacketListenerPlayIn> {
 
+    public long timestamp; // Spigot
+
     public PacketPlayInArmAnimation() {}
 
-    public void a(PacketDataSerializer packetdataserializer) throws IOException {}
+    public void a(PacketDataSerializer packetdataserializer) throws IOException {
+        timestamp = System.currentTimeMillis(); // Spigot
+    }
 
     public void b(PacketDataSerializer packetdataserializer) throws IOException {}
 
