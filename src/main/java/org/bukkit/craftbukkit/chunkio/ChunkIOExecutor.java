178bc73165648ba9bf74626b3cf85d0e609c98ff
Aikar <aikar@aikar.co>
2015-11-15 19:36:37 -0600

    Bump thread count for chunk loading

--- src/main/java/org/bukkit/craftbukkit/chunkio/ChunkIOExecutor.java
+++ src/main/java/org/bukkit/craftbukkit/chunkio/ChunkIOExecutor.java
@@ -7,7 +7,7 @@ import net.minecraft.server.World;
 import org.bukkit.craftbukkit.util.AsynchronousExecutor;
 
 public class ChunkIOExecutor {
-    static final int BASE_THREADS = 1;
+    static final int BASE_THREADS = 2; // PaperSpigot - Bumped value
     static final int PLAYERS_PER_THREAD = 50;
 
     private static final AsynchronousExecutor<QueuedChunk, Chunk, Runnable, RuntimeException> instance = new AsynchronousExecutor<QueuedChunk, Chunk, Runnable, RuntimeException>(new ChunkIOProvider(), BASE_THREADS);
