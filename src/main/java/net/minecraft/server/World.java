08f8e33bddae7397d8b5decb8925fd6a2f74f182
Aikar <aikar@aikar.co>
2016-03-31 19:17:58 -0400

    Do not load chunks for light checks
    Should only happen for blocks on the edge that uses neighbors light level
    (certain blocks). In that case, there will be 3-4 other neighbors to get a light level from.

--- src/main/java/net/minecraft/server/World.java
+++ src/main/java/net/minecraft/server/World.java
@@ -702,6 +702,7 @@ public abstract class World implements IBlockAccess {
                 if (blockposition.getY() >= 256) {
                     blockposition = new BlockPosition(blockposition.getX(), 255, blockposition.getZ());
                 }
+                if (!this.isLoaded(blockposition)) return 0; // Paper
 
                 Chunk chunk = this.getChunkAtWorldCoords(blockposition);
 
